eb50e62f4f608ffd0d64209112fee680cb8e74d9
Marina LaForgia <marina.laforgia@gmail.com>
2019-02-18 10:10:02 -0800

    Calculated Bootstraps for CIs

--- Scripts/Thatch-proj.R
+++ Scripts/Thatch-proj.R
@@ -120,6 +120,7 @@ overdisp(m2.t)
 summary(m2.t) # thatch increases mortality, still higher mortality in Tolerators; no interaction between current grass/drought tolerance or thatch/drought tolerance
 
 ranef(m2.t)
+
 #### M3: Seed set ####
 
 # random effect structure too complex with slope for sublot; missing 20 levels because of low germ/high mort, with the nestedness structure of the previous models there are too many levels of random effects for estimates
@@ -152,6 +153,52 @@ summary(m5)
 #### M6: Grass seed set ####
 
 
+#### Bootstraps CIs ####
+
+df <- expand.grid(Subplot = c("Grass", "No Grass", "Thatch"), PC.F = seq(from = min(trait.w$PC.F), to = max(trait.w$PC.F), length.out = 1000)) 
+
+###
+# Germination
+###
+
+boot1 <- bootMer(m1.t, function(x) predict(x, newdata = df[df$Subplot != "Grass",], type = 'response', allow.new.levels = T, re.form = NA), nsim = 1000, ncpus = 2, verbose = T) # 23/1000 fails
+
+PI.boot <- data.frame(
+           p.germ = apply(boot1$t, 2, function(x) as.numeric(quantile(x, probs = .5, na.rm=TRUE))),
+           p.germ.lo = apply(boot1$t, 2, function(x) as.numeric(quantile(x, probs = .025, na.rm=TRUE))),
+           p.germ.hi = apply(boot1$t, 2, function(x) as.numeric(quantile(x, probs = .975, na.rm=TRUE))))
+
+CI.1 <- merge(PI.boot, df, by = "row.names")
+save(CI.1, file = "Data/Analysis-Output/Bootstrapped-CIs/CI-1.Rdata")
+
+###
+# Mortality
+###
+
+boot2 <- bootMer(m2.t, function(x) predict(x, newdata = df, type = 'response', allow.new.levels = T, re.form = NA), nsim = 1000, verbose = T)
+
+PI.boot <- data.frame(
+           p.mort = apply(boot2$t, 2, function(x) as.numeric(quantile(x, probs = .5, na.rm=TRUE))),
+           p.mort.lo = apply(boot2$t, 2, function(x) as.numeric(quantile(x, probs = .025, na.rm=TRUE))),
+           p.mort.hi = apply(boot2$t, 2, function(x) as.numeric(quantile(x, probs = .975, na.rm=TRUE))))
+
+CI.2 <- merge(PI.boot, df, by = "row.names")
+save(CI.2, file = "Data/Analysis-Output/Bootstrapped-CIs/CI-2.Rdata")
+
+###
+# Seed Set
+###
+
+boot3 <- bootMer(m3.t, function(x) predict(x, newdata = df, type = 'response', allow.new.levels = T, re.form = NA), nsim = 1000, verbose = T) # 
+
+PI.boot <- data.frame(seed = apply(boot3$t, 2, function(x) mean(x)),
+           seed.lo = apply(boot3$t, 2, function(x) as.numeric(quantile(x, probs=.05, na.rm=TRUE))),
+           seed.hi = apply(boot3$t, 2, function(x) as.numeric(quantile(x, probs=.95, na.rm=TRUE))))
+
+CI.3 <- merge(PI.boot, df, by = "row.names")
+save(CI.3, file = "Data/Analysis-Output/Bootstrapped-CIs/CI-3.Rdata")
+
+rm(boot1, boot2, boot3, boot4, boot6, PI.boot)
 
 #### Bootstraps for simulated Lambda ####
 # Dataframe for parameter boots
@@ -183,87 +230,89 @@ df.bt <- expand.grid(Subplot = c("Grass", "No Grass", "Thatch"), PC.F = unique(t
 
 #### Prep: Boots seed avg ####
 
-BS <- list(BS.m, BS.F, BS.g)
-# Create dataframe of parameter means and sds
-params <- c("p.mort.b", "seed.set.b", "p.mort.sd",  "seed.set.sd")
-pm <- as.data.frame(matrix(NA, 18, length(params)))
-names(pm) = params
-pm <- cbind(df.bt, pm)
-
-for(i in 1:2){
-    pm[, i+2] = apply(BS[[i]]$t, 2, function(x) mean(x))
-    pm[, i+4] = apply(BS[[i]]$t, 2, function(x) sd(x))
-}
-
-# grass germ = no grass germ
-pm.gm <- as.data.frame(apply(BS[[3]]$t, 2, function(x) mean(x)))
-pm.gsd <- as.data.frame(apply(BS[[3]]$t, 2, function(x) sd(x)))
-pm.g <- cbind(pm.gm, pm.gsd)
-names(pm.g) <- c("p.germ.b", "p.germ.sd")
-pm.g <- merge(pm.g, df.bt, by = "row.names")
-pm <- merge(pm, pm.g, by = c("Subplot", "PC.F"), all = T)
-pm[pm$Subplot == "Grass",]$p.germ.b <- pm[pm$Subplot == "No Grass",]$p.germ.b
-pm[pm$Subplot == "Grass",]$p.germ.sd <- pm[pm$Subplot == "No Grass",]$p.germ.sd
-pm <- pm[,-7]
-
-# merge with seed survival averages
-pm <- merge(pm, trait.w[,c(1,24)], by = "PC.F")
-pm <- merge(pm, sb.spp, by = "Species")
-
-# Revalue levels for creating scenarios and matching later
-pm$Species <- revalue(pm$Species, c("Agoseris heterophylla" =  "AGHE", "Calycadenia pauciflora" = "CAPA", "Clarkia purpurea" = "CLPU", "Hemizonia congesta" = "HECO", "Lasthenia californica" = "LACA", "Plantago erecta" = "PLER"))
-pm$Subplot <- revalue(pm$Subplot, c("No Grass" = "N", "Grass" = "G", "Thatch" = "T"))
-pm$Scenario <- paste(pm$Subplot, pm$Species, sep = ".")
-Scenario <- pm$Scenario
-later <- pm[,c(1,2,3,11)]
-
-# Create empty Lambda dataframe to fill
-L.sim <- as.data.frame(Scenario)
-sims = 10001
-L.sim[,c(2:sims)] <- NA
-
-# Other dfs for storage and later merger
-g.sim <- L.sim
-m.sim <- L.sim
-s.sim <- L.sim
-F.sim <- L.sim
-
-# Calculate L from random draws of parameter distributions
-for(j in 2:sims){
-  for(i in 1:length(Scenario)){
-    g.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.germ.b"])/100 #germ
-    s.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.sd.surv"])/100 #seed surv
-    #m.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.mort.b"])/100
-    m.sim[i,j] <- rnorm(n = 1, mean = pm[i, "p.mort.b"], sd = pm[i, "p.mort.sd"])
-    F.sim[i,j] <- exp(rnorm(n = 1, mean = pm[i, "seed.set.b"], sd = pm[i, "seed.set.sd"])) - 1 #fecund
-    L.sim[i,j] = s.sim[i,j]*(1 - g.sim[i,j]) + g.sim[i,j]*(1 - m.sim[i,j])*F.sim[i,j]
-  }
-}
-
-# melt all dataframes
-sim.list <- list(L.sim, g.sim, s.sim, m.sim, F.sim)
-for(i in 1:5){ 
-  sim.list[[i]] <- melt(sim.list[[i]])
-}
-
-# merge all dataframes
-names(sim.list[[1]])[3] <- "L"
-names(sim.list[[2]])[3] <- "g"
-names(sim.list[[3]])[3] <- "s"
-names(sim.list[[4]])[3] <- "m"
-names(sim.list[[5]])[3] <- "Fe"
-
-L.sim <- merge(sim.list[[1]], sim.list[[2]], by = c("Scenario", "variable"))
-L.sim <- merge(L.sim, sim.list[[3]], by = c("Scenario", "variable"))
-L.sim <- merge(L.sim, sim.list[[4]], by = c("Scenario", "variable"))
-L.sim <- merge(L.sim, sim.list[[5]], by = c("Scenario", "variable"))
-L.sim <- merge(L.sim, later, by = "Scenario")
-
-B.L <- L.sim[,c(8:10,3:7)]
-
-B.L <- ddply(B.L, .(Species, Subplot, PC.F), summarize, L = mean(L), g = mean(g), sb = mean(s), m = mean(m), Fe = mean(Fe))
-save(B.L, file = "Data/Analysis-Output/Bootstrapped-params//BS-L.Rdata")
-rm(g.sim, s.sim, m.sim, F.sim, params, pm, i, j, later, Scenario, sim.list, sims, BS, L.sim)
+# BS <- list(BS.m, BS.F, BS.g)
+# # Create dataframe of parameter means and sds
+# params <- c("p.mort.b", "seed.set.b", "p.mort.sd",  "seed.set.sd")
+# pm <- as.data.frame(matrix(NA, 18, length(params)))
+# names(pm) = params
+# pm <- cbind(df.bt, pm)
+# 
+# for(i in 1:2){
+#     pm[, i+2] = apply(BS[[i]]$t, 2, function(x) mean(x))
+#     pm[, i+4] = apply(BS[[i]]$t, 2, function(x) sd(x))
+# }
+# 
+# # grass germ = no grass germ
+# pm.gm <- as.data.frame(apply(BS[[3]]$t, 2, function(x) mean(x)))
+# pm.gsd <- as.data.frame(apply(BS[[3]]$t, 2, function(x) sd(x)))
+# pm.g <- cbind(pm.gm, pm.gsd)
+# names(pm.g) <- c("p.germ.b", "p.germ.sd")
+# pm.g <- merge(pm.g, df.bt, by = "row.names")
+# pm <- merge(pm, pm.g, by = c("Subplot", "PC.F"), all = T)
+# pm[pm$Subplot == "Grass",]$p.germ.b <- pm[pm$Subplot == "No Grass",]$p.germ.b
+# pm[pm$Subplot == "Grass",]$p.germ.sd <- pm[pm$Subplot == "No Grass",]$p.germ.sd
+# pm <- pm[,-7]
+# 
+# # merge with seed survival averages
+# pm <- merge(pm, trait.w[,c(1,24)], by = "PC.F")
+# pm <- merge(pm, sb.spp, by = "Species")
+# 
+# # Revalue levels for creating scenarios and matching later
+# pm$Species <- revalue(pm$Species, c("Agoseris heterophylla" =  "AGHE", "Calycadenia pauciflora" = "CAPA", "Clarkia purpurea" = "CLPU", "Hemizonia congesta" = "HECO", "Lasthenia californica" = "LACA", "Plantago erecta" = "PLER"))
+# pm$Subplot <- revalue(pm$Subplot, c("No Grass" = "N", "Grass" = "G", "Thatch" = "T"))
+# pm$Scenario <- paste(pm$Subplot, pm$Species, sep = ".")
+# Scenario <- pm$Scenario
+# later <- pm[,c(1,2,3,11)]
+# 
+# # Create empty Lambda dataframe to fill
+# L.sim <- as.data.frame(Scenario)
+# sims = 10001
+# L.sim[,c(2:sims)] <- NA
+# 
+# # Other dfs for storage and later merger
+# g.sim <- L.sim
+# m.sim <- L.sim
+# s.sim <- L.sim
+# F.sim <- L.sim
+# 
+# # Calculate L from random draws of parameter distributions
+# for(j in 2:sims){
+#   for(i in 1:length(Scenario)){
+#     g.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.germ.b"])/100 #germ
+#     s.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.sd.surv"])/100 #seed surv
+#     #m.sim[i,j] <- rbinom(n = 1, size = 100, prob = pm[i, "p.mort.b"])/100
+#     m.sim[i,j] <- rnorm(n = 1, mean = pm[i, "p.mort.b"], sd = pm[i, "p.mort.sd"])
+#     F.sim[i,j] <- exp(rnorm(n = 1, mean = pm[i, "seed.set.b"], sd = pm[i, "seed.set.sd"])) - 1 #fecund
+#     L.sim[i,j] = s.sim[i,j]*(1 - g.sim[i,j]) + g.sim[i,j]*(1 - m.sim[i,j])*F.sim[i,j]
+#   }
+# }
+# 
+# # melt all dataframes
+# sim.list <- list(L.sim, g.sim, s.sim, m.sim, F.sim)
+# for(i in 1:5){ 
+#   sim.list[[i]] <- melt(sim.list[[i]])
+# }
+# 
+# # merge all dataframes
+# names(sim.list[[1]])[3] <- "L"
+# names(sim.list[[2]])[3] <- "g"
+# names(sim.list[[3]])[3] <- "s"
+# names(sim.list[[4]])[3] <- "m"
+# names(sim.list[[5]])[3] <- "Fe"
+# 
+# L.sim <- merge(sim.list[[1]], sim.list[[2]], by = c("Scenario", "variable"))
+# L.sim <- merge(L.sim, sim.list[[3]], by = c("Scenario", "variable"))
+# L.sim <- merge(L.sim, sim.list[[4]], by = c("Scenario", "variable"))
+# L.sim <- merge(L.sim, sim.list[[5]], by = c("Scenario", "variable"))
+# L.sim <- merge(L.sim, later, by = "Scenario")
+# 
+# B.L <- L.sim[,c(8:10,3:7)]
+# 
+# B.L <- ddply(B.L, .(Species, Subplot, PC.F), summarize, L = mean(L), g = mean(g), sb = mean(s), m = mean(m), Fe = mean(Fe))
+# 
+# save(B.L, file = "Data/Analysis-Output/Bootstrapped-params/BS-L.Rdata")
+# 
+# rm(g.sim, s.sim, m.sim, F.sim, params, pm, i, j, later, Scenario, sim.list, sims, BS, L.sim)
 
 
 #### M7: Boot Lambda ####
@@ -288,9 +337,10 @@ hist(resid(m5.t))
 
  #### Plot Germination ####
 
-germ.t.sum <- summarySE(dem, measurevar = "p.germ", groupvars = c("Subplot","Species","PC.F"), na.rm = T)
+germ.t.sum <- summarySE(dem[dem$Subplot != "Grass",], measurevar = "p.germ", groupvars = c("Subplot","Species","PC.F"), na.rm = T)
 
 ggplot(germ.t.sum, aes(x = PC.F, y = p.germ, col = Subplot, group = Subplot)) +
+  geom_ribbon(data = CI.1, aes(ymin = p.germ.lo, ymax = p.germ.hi), alpha = 0.1, linetype = 0) +
   geom_point() +
   theme_classic() +
   theme(legend.title = element_blank(), 
@@ -303,7 +353,7 @@ ggplot(germ.t.sum, aes(x = PC.F, y = p.germ, col = Subplot, group = Subplot)) +
         axis.line = element_blank(),
         panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
   labs(y = "Germination Rate", x = "Drought Tolerance") +
-  geom_smooth(method = "lm", se = F) + 
+  geom_line(data = CI.1, aes(y = p.germ), size = 1) +
   ylim(.1,1) +
   scale_color_viridis_d() 
 
@@ -311,7 +361,10 @@ ggplot(germ.t.sum, aes(x = PC.F, y = p.germ, col = Subplot, group = Subplot)) +
 
 dem.sum <- summarySE(dem, measurevar = "p.mort", groupvars = c("Subplot","Species","PC.F"), na.rm = T)
 
+dem.sum$Subplot <- factor(dem.sum$Subplot, levels = c("No Grass", "Grass", "Thatch"))
+
 ggplot(dem.sum, aes(x = PC.F, y = p.mort, col = Subplot, group = Subplot)) +
+  geom_ribbon(data = CI.2, aes(ymin = p.mort.lo, ymax = p.mort.hi), alpha = 0.1, linetype = 0) +
   geom_point() +
   theme_classic() +
   theme(legend.title = element_blank(), 
@@ -324,15 +377,17 @@ ggplot(dem.sum, aes(x = PC.F, y = p.mort, col = Subplot, group = Subplot)) +
         axis.line = element_blank(),
         panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
   labs(y = "Moratlity Rate", x = "Drought Tolerance") +
-  geom_smooth(method = "lm", se = F) + 
+  geom_line(data = CI.2, aes(y = p.mort), size = 1) +
   scale_color_viridis_d()
 
 
 #### Plot Seed set ####
 
 flo.seed.sum <- summarySE(flo.seed, measurevar = "n.seed.ind", groupvars = c("Subplot","Species","PC.F"), na.rm = T)
+flo.seed.sum$Subplot <- factor(flo.seed.sum$Subplot, levels = c("No Grass", "Grass", "Thatch"))
 
 ggplot(flo.seed.sum, aes(x = PC.F, y = n.seed.ind, col = Subplot, group = Subplot)) +
+  geom_ribbon(data = CI.3, aes(y = NULL, ymin = exp(seed.lo) - 1, ymax = exp(seed.hi) - 1), alpha = 0.1, linetype = 0) +
   geom_point() +
   theme_classic() +
   theme(legend.title = element_blank(), 
@@ -345,7 +400,7 @@ ggplot(flo.seed.sum, aes(x = PC.F, y = n.seed.ind, col = Subplot, group = Subplo
         axis.line = element_blank(),
         panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
   labs(y = "Seed Set", x = "Drought Tolerance") +
-  geom_smooth(method = "lm", se = F) + 
+  geom_line(data = CI.3, aes(y = exp(seed) - 1), size = 1) +
   scale_color_viridis_d()
 
 #### Plot Lambda (plot) ####
